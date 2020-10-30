04d7c7209e0e957a42b27554a94e27773aedaa49
Deanna Norman <deannaleenorman@msn.com>
2020-10-30 01:30:25 -0700

    final commit

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-console.log("It's working!")
 
 function handleGetData(event){
-  event.preventDefault();
-  // Load in the value of the search textbox:
-  const searchText = $("#cityname").val()
-  $.ajax({url: `api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=99df413c60279878184277e08a2c6863`})
+    event.preventDefault();
+    
+    const cityName = $("#city").val()
+    $.ajax(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=99df413c60279878184277e08a2c6863`)
     .then(
-      (data) => {
-        console.log(data);
-        $("#weatherFor").text(data.Weatherfor)
-        $("temperature").text(data.Temperature)
-        $("feelsLike").text(data.FeelsLike)
-        $("weather").text(data.Weather)
-        
-      },
-      (error) => {
-        console.log("bad request: ", error)
-      }
-    )
+        (data) => {
+            console.log(data);
+            $("#name").text(data.name)
+            $("#temp").text(data.temp)
+            $("#feels").text(data.feels_like)
+            $("#weather").text(data.weather)
+            
+        },
+        (error) => {
+            console.log("bad request: ", error)
+        }
+        )
+        console.log("It's working!")
 }
 $('form').on("submit", handleGetData)
 
